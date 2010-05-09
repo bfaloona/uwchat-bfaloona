a169d3aa4b0e29d52f3a98295987560bbeb02842
Brandon Faloona <brandon@faloona.net>
2010-05-08 22:30:53 -0700

    message delivery

--- lib/uwchat/server.rb
+++ lib/uwchat/server.rb
@@ -4,18 +4,33 @@ module UWChat
 
   class AuthenticationFailed < RuntimeError; end
 
+  # a chat client connection
+  class Connection
+
+    attr_reader :port, :sock, :username
+
+    def initialize( port, sock, username=nil )
+      @port = port
+      @sock = sock
+      @username = username
+    end
+
+    def to_s
+      "#{username || port}"
+    end
+  end
+
   class Server < GServer
     attr_reader :clients
 
     def initialize( port=36963, *args )
-      Struct.new("Client", :port, :username)
       @clients = []
       super(port, *args)
     end
 
-    def new_client( port, username=nil )
+    def new_client( port, sock, username=nil )
       log( "Adding client on port: #{port}" )
-      client = Struct::Client.new( port, username )
+      client = Connection.new( port, sock, username )
       @clients.push client
       client
     end
@@ -26,7 +41,7 @@ module UWChat
     end
 
     def connecting(client)
-      new_client( client.peeraddr[1] )
+      new_client( client.peeraddr[1], client )
       super
     end
 
@@ -63,9 +78,31 @@ module UWChat
       client
     end
 
-    def welcome_client( io )
-      client = find_client_by_socket( io )
-      io.puts "Welcome #{client.username}"
+    def welcome_client( sock )
+      client = find_client_by_socket( sock )
+      sock.puts "Welcome #{client.username}"
+    end
+
+    # send message from sender to recipient
+    def private(msg, sender, recipient)
+      recipient.sock.puts "#{sender}: #{msg}"
+      log( "[private] #{sender} to #{recipient}: #{msg}")
+    end
+
+    # send message to all clients
+    def broadcast( msg )
+      @clients.each do |client|
+        client.sock.puts "Announce: #{msg}"
+      end
+      log( "[broadcast] #{msg}" )
+    end
+
+    # send message to all clients except sender
+    def message( msg, sender )
+      @clients.each do |client|
+        client.sock.puts "#{sender}: #{msg}" unless sender.port == client.port
+      end
+      log( "[message] #{sender}: #{msg}" )
     end
   end
 
--- spec/server_spec.rb
+++ spec/server_spec.rb
@@ -44,7 +44,8 @@ describe UWChat::Server do
   describe "Multi-Client management" do
 
     it "should add clients to @clients as they connect" do
-      @server.new_client( 1, 'steve' )
+      sock = stub('socket')
+      @server.new_client( 1, sock, 'steve' )
 
       client_socket = stub(:peeraddr => [nil, 36969])
 
@@ -56,8 +57,8 @@ describe UWChat::Server do
     end
 
     it "should remove clients at time of disconnect" do
-      @server.new_client( 1, 'steve' )
-      @server.new_client( 10_000, 'penelope' )
+      @server.new_client( 1, mock('steve_sock'), 'steve' )
+      @server.new_client( 10_000, mock('penelope_sock'), 'penelope' )
       @server.clients.size.should == 2
 
       @server.disconnecting( 10_000 )
@@ -65,21 +66,23 @@ describe UWChat::Server do
       client.port.should == 1
       client.username.should == 'steve'
       @server.clients.size.should == 1
-    end
+    end 
 
     it "should add clients to @clients via add_client()" do
+      sock = stub('socket')
       @server.clients.size.should == 0
-      @server.new_client( 1, 'steve' )
-      @server.new_client( 36966, 'larry' )
+      @server.new_client( 1, sock, 'steve' )
+      @server.new_client( 36966, sock, 'larry' )
       @server.clients.size.should == 2
       @server.clients.first.port == 1
       @server.clients.last.username == 'larry'
     end
 
     it "should remove clients from @clients via remove_client()" do
-      @server.new_client( 1, 'steve' )
-      @server.new_client( 36966, 'larry' )
-      @server.new_client( 100, 'sue' )
+      sock = stub('socket')
+      @server.new_client( 1, sock, 'steve' )
+      @server.new_client( 36966, sock, 'larry' )
+      @server.new_client( 100, sock, 'sue' )
       @server.clients.size.should == 3
 
       # Act
@@ -91,15 +94,78 @@ describe UWChat::Server do
     end
 
     it "should be able to find client by socket" do
-      @server.new_client( 1, 'steve' )
-      @server.new_client( 36966, 'larry' )
-      @server.new_client( 100, 'sue' )
+      @server.new_client( 1, mock('steve_sock'), 'steve' )
+      @server.new_client( 36966, mock('larry_sock'), 'larry' )
+      @server.new_client( 100, mock('sue_sock'), 'sue' )
 
       mock_sock = mock()
       mock_sock.should_receive( :addr ).and_return( [nil, 36966] )
       client = @server.find_client_by_socket( mock_sock )
       client.port.should == 36966
       client.username.should == 'larry'
+    end
+
+  end
+
+  describe "Message delivery" do
+
+    it "should send and log broadcast messages to all users" do
+      # Arrange
+      msg = "Broadcast Message"
+      expected_msg_recieved = "Announce: #{msg}"
+      mock_sock1 = mock( 'socket1' )
+      mock_sock2 = mock( 'socket2' )
+      mock_sock3 = mock( 'socket3' )
+      @server.new_client( 1, mock_sock1, 'steve' )
+      @server.new_client( 36966, mock_sock2, 'larry' )
+      @server.new_client( 100, mock_sock3, 'sue' )
+
+      # Expect
+      mock_sock1.should_receive( :puts ).with( expected_msg_recieved )
+      mock_sock2.should_receive( :puts ).with( expected_msg_recieved )
+      mock_sock3.should_receive( :puts ).with( expected_msg_recieved )
+      @server.should_receive( :log ).with( "[broadcast] #{msg}" )
+
+      # Act
+      @server.broadcast( msg )
+    end
+
+    it "should send and log private messages to one user" do
+
+      # Arrange
+      msg = "Private Message"
+      sender = 'larry'
+      mock_sock = mock( 'socket' )
+
+      # Expect
+      mock_sock.should_receive( :puts ).with( /^#{sender}: #{msg}$/ )
+      recipient = stub(:port => 20_000, :sock => mock_sock, :username => 'recipient')
+      @server.should_receive( :log ).with( "[private] #{sender} to #{recipient}: #{msg}" )
+
+      # Act
+      @server.private(msg, sender, recipient)
+    end
+
+    it "should send normal messages to every other user" do
+      # Arrange
+      msg = "Chat Message"
+      expected_msg_recieved = "larry: #{msg}"
+
+      mock_sock_sender = mock( 'socket_sender' )
+      mock_sock1 = mock( 'socket1' )
+      mock_sock2 = mock( 'socket2' )
+      sender = @server.new_client( 100, mock_sock_sender, 'larry' )
+      @server.new_client( 1, mock_sock1, 'steve' )
+      @server.new_client( 36966, mock_sock2, 'susan' )
+
+      # Expect
+      mock_sock_sender.should_not_receive( :puts )
+      mock_sock1.should_receive( :puts ).with( expected_msg_recieved )
+      mock_sock2.should_receive( :puts ).with( expected_msg_recieved )
+      @server.should_receive( :log ).with( "[message] larry: #{msg}" )
+
+      # Act
+      @server.message( msg, sender )
     end
 
   end
