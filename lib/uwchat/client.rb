9c302cd74a3a9cb87683d4c215d1bb2d2727c08e
Brandon Faloona <brandon@faloona.net>
2010-05-11 02:02:53 -0700

    client!

--- lib/uwchat/client.rb
+++ lib/uwchat/client.rb
@@ -1,9 +1,61 @@
 module UWChat
 
   class Client
-    def initialize( )
+
+    # TCPSocket connection to the server
+    attr_reader :sock
+
+    def initialize( port=36963 )
+      @port = port
+    end
+
+    def start
+      begin
+        connect()
+        listener = Thread.new do
+          loop do
+            scribe( @sock )
+          end
+        end
+        loop do
+          send( @sock )
+        end
+      rescue Errno::ECONNREFUSED => e
+        puts "A network error occurred (#{e.to_s}). Goodbye."
+      rescue => e
+        puts "An error occured: #{e.to_s}"
+      ensure
+        listener.kill if listener
+        disconnect
+      end
+    end
+
+    def connect
+      @sock = TCPSocket.new( 'localhost', @port )
+      welcome_msg = @sock.gets
+      puts welcome_msg
+    end
+
+    def scribe( socket )
+      message = socket.gets.chomp
+
+      if message && message.match( /^\^\^\[([\w]+)\](?:\[(.*)\])$/)
+        cmd = Regexp.last_match(1)
+        data = Regexp.last_match(2)
+        process_command( cmd, data )
+      elsif message
+        puts message
+      end
+    end
+
+    def await_input 
 
     end
+
+    def disconnect
+
+    end
+
   end
 
 end
---
+++ spec/client_spec.rb
@@ -0,0 +1,79 @@
+require 'uwchat'
+
+describe UWChat::Client do
+
+  describe "Project structure" do
+
+    it "should contain required classes" do
+      UWChat::Client.should be_true
+    end
+
+  end
+
+  describe "Network properties" do
+
+    before( :each ) do
+      # Arrange
+      @client = UWChat::Client.new
+      @mock_sock = mock( 'socket' )
+    end
+
+    it "should connect to port 36963 by default" do
+      # Expect
+      TCPSocket.should_receive( :new ).with( 'localhost', 36963 ).and_return( StringIO.new )
+
+      # Act
+      @client.connect
+    end
+
+    it "should display the server welcome message" do
+      # Expect
+      TCPSocket.should_receive( :new ).and_return( @mock_sock )
+      @mock_sock.should_receive( :gets ).and_return( 'Welcome user' )
+      @client.should_receive( :puts ).with( 'Welcome user' )
+
+      # Act
+      @client.connect
+    end
+
+    it "should display normal text sent from the server" do
+      # Expect
+      @mock_sock.should_receive( :gets ).and_return( "larry: i'm hungry" )
+      @client.should_receive( :puts ).with( "larry: i'm hungry" )
+
+      # Act
+      @client.scribe( @mock_sock )
+    end
+
+    it "should process commands from the server" do
+      # Expect
+      @mock_sock.should_receive( :gets ).and_return( "^^[boot][you are mean]" )
+      @client.should_receive( :process_command ).with( "boot", "you are mean" )
+
+      # Act
+      @client.scribe( @mock_sock )
+    end
+
+    it "should send messages to the server" do
+      # Expect
+      @mock_sock.should_receive( :gets ).and_return( "I'm a nut!\n")
+      @client.should_receive( :puts ).with( "I'm a nut!")
+
+      # Act
+      @client.scribe( @mock_sock )
+    end
+
+    it "should quit gracefully if a network error is encountered" do
+      # Expect
+      TCPSocket.should_receive( :new ).and_return( @mock_sock )
+      @mock_sock.should_receive( :gets ).and_raise( Errno::ECONNREFUSED )
+      @client.should_receive( :puts ).with( 'A network error occurred. Goodbye.' )
+      @client.should_receive( :disconnect )
+
+      # Act
+      @client.start()
+    end
+
+  end
+
+end
